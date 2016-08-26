660d5e6d39a7f8266d734bca598eb8032539fbfa
mtso <mtso@mail.com>
2016-08-26 15:39:37 -0700

    Add socket.io and set up index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+var app  = require('express')();
+var http = require('http').Server(app);
+var io   = require('socket.io')(http);
+
+app.get('/', function(req, res) {
+  res.send('<!doctype html><html><body><h1>conlan server</h1><script src="https://cdn.jsdelivr.net/socket.io-client/1.3.2/socket.io.min.js"></script><script>var socket = io();</script></body><html>');
+});
+
+http.listen(3000, function() {
+  console.log('Listening on *:3000');
+});
+
+io.on('connection', function(socket) {
+  console.log('A user connected.');
+});
